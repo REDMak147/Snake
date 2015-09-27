83537d071996e34058f7d500a27df085da6bf5b3
REDkID <redmak147@gmail.com>
2015-09-27 15:49:53 +0300

    Инкапсуляция

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,22 +10,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            { Console.WriteLine(i); }
+            numList.Remove(0);
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-        Console.SetCursorPosition(x, y);
-        Console.Write(sym);
-}
-}
+     }
 }
